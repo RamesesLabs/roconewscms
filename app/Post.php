ce92074e3f89a2a37f5dd0b332cbeaea6d857b5a
rameseslabs <rameseslabs@gmail.com>
2020-01-11 14:32:25 -0500

    Added Soft Deletes to Posts Controller

--- app/Post.php
+++ app/Post.php
@@ -1,8 +1,12 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Post extends Model
 {
+    use SoftDeletes;
+
+
     protected $fillable = [
       'title', 'description', 'content', 'image', 'published_at'
     ];
